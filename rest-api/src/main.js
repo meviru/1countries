57e9ba0d5410d300c2bf877bb5aee75ee8547f92
Viral Patel <viralpatel508@gmail.com>
2022-05-04 12:29:52 +0530

    Delete rest-api directory

--- rest-api/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-import App from "./App.vue";
-import router from "./components/router/index";
-import Toast from "vue-toastification";
-
-createApp(App).use(router).use(ElementPlus).use(Toast).mount("#app");
